a724ae821cff814ca0bec75218c04e20ef35979f
jackson-veloso <jackson.veloso@hotmail.com>
2022-06-24 19:04:08 -0300

    aula 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/layout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+  <main>
+    <Layout>
+      <div>
+        <img source="https://avatars.githubusercontent.com/u/39008435?v=4" alt="Avatar of user" />
+        <h1>Matheus Benites</h1>
+        <h3>Username: </h3>
+        <span>benits</span>
+        <div>
+          <h4>Followers</h4>
+          <span>5</span>
+        </div>
+        <div>
+          <h4>Starreds</h4>
+          <span>5</span>
+        </div>
+        <div>
+          <h4>Followings</h4>
+          <span>5</span>
+        </div>
+      </div>
+      <div>Repositores</div>
+      <div>Starreds</div>
+    </Layout>
+  </main>
   );
 }
 
